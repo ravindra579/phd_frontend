4ff396c002b16d17b27c89a7f3ce539294e69581
ravindra <ravindrareddy1217@gmail.com>
2022-12-13 16:02:08 +0530

    outline structure

---
+++ src/main.js
@@ -0,0 +1,86 @@
+import "./App.css"
+function Main() {
+    return(
+        <div>
+            <div className="top">
+                <div className="flex">
+                    <div className="width50">
+                        Date
+                    </div>
+                    <div className="width50">
+                        Time
+                    </div>
+                </div>
+            </div>
+            <div className="middle">
+                <div class="row">
+                  <div class="column">
+                    <div class="card">
+                        <p>
+                            rijbf
+                        </p>
+                        <p>
+                            r3oifh
+                        </p>
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="card">
+                    <p>
+                            rijbf
+                        </p>
+                        <p>
+                            r3oifh
+                        </p>                        
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="card">
+                    <p>
+                            rijbf
+                        </p>
+                        <p>
+                            r3oifh
+                        </p>                        
+                    </div>
+                  </div>
+                  <div class="column">
+                    <div class="card">
+                    <p>
+                            rijbf
+                        </p>
+                        <p>
+                            r3oifh
+                        </p>                        
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div className="bottom">
+            <div class="row">
+                <div class="column1">
+                    <div class="card">
+                        <p>
+                            rijbf
+                        </p>
+                        <p>
+                            r3oifh
+                        </p>
+                    </div>
+                  </div>
+                  <div class="column1">
+                    <div class="card">
+                    <p>
+                            rijbf
+                        </p>
+                        <p>
+                            r3oifh
+                        </p>                        
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Main;
